7a59f687821e2692e403181dc840bf06c7112327
samiksha-satthy <satthy.samiksha@gmail.com>
2025-12-02 18:22:33 -0500

    updated wardrobe endpoints

--- backend/routes/wardrobeRoutes.js
+++ backend/routes/wardrobeRoutes.js
@@ -33,106 +33,114 @@ const upload = multer({ storage: storage });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function authMiddleware(req, res, next){
-  if (req.isAuthenticated){
-    return next();}
-   
-  else 
-    return res.send("authentication error!!")
+function authMiddleware(req, res, next) {
+  if (req.isAuthenticated) {
+    return next();
+  } else return res.send("authentication error!!");
 }
 
-router.post("/upload-item", authMiddleware, upload.single("myFile"), async (req, res) => {
-  try {
+router.post(
+  "/upload-item",
+  authMiddleware,
+  upload.single("myFile"),
+  async (req, res) => {
+    try {
+      if (!req.file) {
+        res.send("no file uploaded ");
+      }
 
-    console.log("hihihih");
-    if (!req.file) {
-      res.send("no file uploaded ");
+      const response = await pool.query(
+        "INSERT INTO items (user_id, item_name, image_path) VALUES ($1, $2, $3)",
+        [req.user.id, req.body.name, req.file.filename]
+      );
+
+      res.send(response);
+    } catch (error) {
+      res.send(error);
     }
-
-    const response = await pool.query(
-      "INSERT INTO wardrobe_items (user_id, item_name, image_path) VALUES ($1, $2, $3)",
-      [req.user.id, req.body.name, req.file.filename]
-    );
-
-    console.log("after query");
-
-    if (!req.user || !req.user.name) {
-      console.error("User object missing:", req.user);
-      return res.status(401).send("User not authenticated or name missing");
-    }
-
-    console.log("hey there")
-    var first_part = req.user.name.split(" ")[0].toLowerCase();
-    console.log(first_part)
-
-    const scriptPath = path.join(
-      __dirname,
-      "../gemini_embeddings",
-      "cohere_test.py"
-    );
-    const pythonPath = path.join(
-      __dirname,
-      "../gemini_embeddings/venv",
-      "Scripts",
-      "python.exe"
-    );
-
-    var pythonOutput = "";
-
-    console.log("scriptPath:", scriptPath);
-    console.log("pythonPath:", pythonPath);
-    console.log("cwd:", process.cwd());
-
-    var image_path = `../uploads/${req.user.id}/${req.file.originalname}`
-    console.log(image_path)
-
-    const save_to_db = spawn(pythonPath, [
-      "-u",
-      scriptPath,
-      image_path,
-      first_part,
-      req.user.id,
-    ], { cwd: path.resolve(__dirname, "../gemini_embeddings") });
-
-
-
-    console.log("hellooooo")
-
-
-
-    save_to_db.stdout.on("data", (data) => {
-      pythonOutput += data.toString();
-      console.log(`Output: ${data.toString()}`);
-    });
-
-    save_to_db.stderr.on("data", (data) => {
-      console.error(`Error: ${data.toString()}`);
-    });
-
-    save_to_db.on("error", (err) => {
-      console.error("Failed to start Python process:", err);
-    });
-
-    save_to_db.on("close", (code) => {
-      console.log(`Python process exited with code ${code}`);
-      res.send({
-        status: code === 0 ? "success" : "error",
-        output: pythonOutput.trim(),
-      });
-    });
-  } catch (error) {
-    res.send(error);
   }
-});
+);
 
 router.get("/items", async (req, res) => {
   try {
+    if (req.isAuthenticated()) {
+      const response = await pool.query(
+        "SELECT image_path FROM users WHERE user_id=($1)",
+        [req.user.id]
+      );
+    }
   } catch (error) {}
 });
 
 router.get("/delete-item/:id", async (req, res) => {
   try {
   } catch (error) {}
+});
+
+router.patch("/update-type/:id", async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const update = await pool.query(
+        "UPDATE items SET type=($1) WHERE id=($2)",
+        [req.body.type, req.params.id]
+      );
+
+      const response = await pool.query("SELECT image_path FROM items WHERE id=($1)", [req.params.id])
+
+      var first_part = req.user.name.split(" ")[0].toLowerCase();
+      console.log(first_part);
+
+      const scriptPath = path.join(
+        __dirname,
+        "../gemini_embeddings",
+        "cohere_test.py"
+      );
+      const pythonPath = path.join(
+        __dirname,
+        "../gemini_embeddings/venv",
+        "Scripts",
+        "python.exe"
+      );
+
+      var pythonOutput = "";
+
+      console.log("scriptPath:", scriptPath);
+      console.log("pythonPath:", pythonPath);
+      console.log("cwd:", process.cwd());
+
+
+      const save_to_db = spawn(
+        pythonPath,
+        ["-u", scriptPath, response.rows[0].image_path, first_part, req.user.id],
+        { cwd: path.resolve(__dirname, "../gemini_embeddings") }
+      );
+
+      save_to_db.stdout.on("data", (data) => {
+        pythonOutput += data.toString();
+        console.log(`Output: ${data.toString()}`);
+      });
+
+      save_to_db.stderr.on("data", (data) => {
+        console.error(`Error: ${data.toString()}`);
+      });
+
+      save_to_db.on("error", (err) => {
+        console.error("Failed to start Python process:", err);
+      });
+
+      save_to_db.on("close", (code) => {
+        console.log(`Python process exited with code ${code}`);
+        res.send({
+          status: code === 0 ? "success" : "error",
+          output: pythonOutput.trim(),
+        });
+      });
+    } else {
+      res.send("not authenticated");
+    }
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 export default router;
